66136b06bb90f940f6700f9645d505f17e5aa2d3
Chris <chris.wittyname@gmail.com>
2020-03-08 22:31:19 +1300

    Updated AuthService to use promises

--- src/services/Auth.js
+++ src/services/Auth.js
@@ -9,48 +9,50 @@ class AuthService {
     'https://www.googleapis.com/auth/drive'
   ];
 
-  init = callback => {
-    console.log(this.API_KEY)
-    gapi.load('client:auth2', () => {
-      this.onLoadSuccess(callback);
-    });
+  init = () => {
+    return new Promise(resolve => {
+      gapi.load('client:auth2', async () => {
+        await this.onLoadSuccess();
+        await this.onInitSucceed();
+        resolve();
+      });
+    })
   };
 
   onLoadSuccess = async callback => {
-    await gapi.client.init({
+    return await gapi.client.init({
       apiKey: this.API_KEY,
       clientId: this.CLIENT_ID,
       scope: this.SCOPES.join(',')
     });
-
-    this.onInitSucceed(callback);
   };
 
-  onInitSucceed = callback => {
+  onInitSucceed = async () => {
     const isSignedIn = gapi.auth2.getAuthInstance().isSignedIn.get();
 
     if (isSignedIn) {
-      this.loadDriveApi(isSignedIn, callback);
+      return await this.loadDriveApi();
     } else {
-      gapi.auth2.getAuthInstance().isSignedIn.listen(isSignedIn => {
-        this.loadDriveApi(isSignedIn, callback);
+      return await this.signIn();
+    }
+  };
+
+  signIn = async () => {
+    return new Promise(resolve => {
+      gapi.auth2.getAuthInstance().isSignedIn.listen(async isSignedIn => {
+        await this.loadDriveApi();
+        resolve();
       });
       gapi.auth2.getAuthInstance().signIn();
-    }
+    })
   };
 
   onInitError = error => {
     console.error(error)
   };
 
-  loadDriveApi = (result, callback) => {
-    gapi.client.load('drive', 'v3', () => {
-      this.driveApiLoaded(result, callback);
-    });
-  };
-
-  driveApiLoaded = (result, callback) => {
-    callback(result);
+  loadDriveApi = async () => {
+    return await gapi.client.load('drive', 'v3');
   };
 }
 
