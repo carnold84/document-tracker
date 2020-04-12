d7ff9ce577f4cc4068d0707b335679b48b646865
Chris <chris.wittyname@gmail.com>
2020-04-12 12:38:18 +1200

    Updates

--- src/theme.js
+++ src/theme.js
@@ -2,7 +2,12 @@ export default {
   color1: '#1a1e21',
   color2: '#242a2f',
   color3: '#313C43',
+  color4: '#44525c',
   colorAlt1: '#e5e1De',
+  fontFamilyPrimary: '"Lora", serif',
+  fontFamilySecondary: '"Lato", sans-serif',
   text1: '#e5e1De',
   text2: '#848D91',
+  textAlt1: '#1a1e21',
+  textAlt2: '#242a2f',
 };
